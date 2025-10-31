653318a342bd31be5b37138b45a0c22a55784b6e
Jane Fan <jane.fan@safetyculture.io>
2025-10-31 12:01:03 +1100

    Updates

---
+++ assets/illustration-CO_DpfI-.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CU76a2Tg.js";const o=({children:r,...s})=>t.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",role:"presentation",...s,children:r});export{o as e};
--- assets/illustration-DFmV-oXw.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BRY32I6b.js";const o=({children:r,...s})=>t.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",role:"presentation",...s,children:r});export{o as e};
